7d9705d3d10c7f405f426b8c314440f061821e6b
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-21 22:10:27 -0300

    FEAT: add queue methods

--- queue/queue.go
+++ queue/queue.go
@@ -16,8 +16,8 @@ func (q *Queue[T]) Enqueue(element T) {
 
 // Dequeue removes and returns the first element added to the queue
 func (q *Queue[T]) Dequeue() (T, bool) {
-	var zero T
 	if q.IsEmpty() {
+		var zero T
 		return zero, false
 	}
 	item := q.items[0]
@@ -26,7 +26,23 @@ func (q *Queue[T]) Dequeue() (T, bool) {
 
 }
 
+// FrontQueue returns the element at the front of the queue
+func (q *Queue[T]) FrontQueue() (T, bool) {
+	if q.IsEmpty() {
+		var zero T
+		return zero, false
+	}
+
+	return q.items[0], true
+
+}
+
 // IsEmpty returns true if the queue is empty
 func (q *Queue[T]) IsEmpty() bool {
 	return len(q.items) == 0
+}
+
+// Size returns the numbers of elements in the queue
+func (q *Queue[T]) Size() int {
+	return len(q.items)
 }
