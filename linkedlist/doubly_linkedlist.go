4372f5be3f995e6def85f1dbde8be430dac1b662
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-16 22:23:09 -0300

    FEAT: adds GetElementAt and Insert methods

--- linkedlist/doubly_linkedlist.go
+++ linkedlist/doubly_linkedlist.go
@@ -28,3 +28,65 @@ type DoublyLinkedList[T any] struct {
 func NewDoublyLinkedList[T any]() *DoublyLinkedList[T] {
 	return &DoublyLinkedList[T]{}
 }
+
+// GetElementAt returns an element that is at a specif position
+func (dll *DoublyLinkedList[T]) GetElementAt(position int) (*DoublyNode[T], bool) {
+
+	if position == 0 && position <= dll.Count {
+
+		node := dll.Head
+		for range position {
+			node = node.Next
+		}
+
+		return node, true
+
+	}
+
+	var zero T
+	return &DoublyNode[T]{Element: zero}, false
+
+}
+
+// Insert inserts an element at any position in the doubly linked list
+func (dll *DoublyLinkedList[T]) Insert(element T, position int) bool {
+
+	if position >= 0 && position <= dll.Count {
+
+		node := NewDoublyNode(element)
+		current := dll.Head
+
+		if position == 0 {
+			if dll.Head == nil {
+				dll.Head = node
+				dll.Tail = node
+			}
+			node = dll.Head
+			current.Prev = node
+			dll.Head = node
+		}
+
+		if position == dll.Count {
+			current = dll.Tail
+			current.Next = node
+			node.Prev = current
+			dll.Tail = node
+		}
+
+		previous, ok := dll.GetElementAt(position - 1)
+		if !ok {
+			return false
+		}
+		current = previous.Next
+		node.Next = current
+		previous.Next = node
+		current.Prev = node
+		node.Prev = previous
+
+		dll.Count++
+		return true
+	}
+
+	return false
+
+}
