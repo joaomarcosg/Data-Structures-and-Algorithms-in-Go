14c38766811a181c34b1211ff197af3ba5ea1e63
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-04 21:28:39 -0300

    FEAT: adds Dictionary initial structure

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,1 +1,13 @@
 package dictionary
+
+// Dictionary represents a dictionary
+type Dictionary[T comparable] struct {
+	Table map[string]T
+}
+
+// NewDictionary creates and returns a new dictionary
+func NewDictionary[T comparable]() *Dictionary[T] {
+	return &Dictionary[T]{
+		Table: make(map[string]T),
+	}
+}
