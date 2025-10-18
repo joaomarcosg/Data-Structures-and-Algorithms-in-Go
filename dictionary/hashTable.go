e0db8c670e40037a981222cb00b3187ce167aaf9
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-18 17:26:12 -0300

    FEAT: adds initial structure

--- dictionary/hashTable.go
+++ dictionary/hashTable.go
@@ -1,1 +1,13 @@
 package dictionary
+
+// HashTable represents a hash map
+type HashTable[T comparable] struct {
+	Table map[string]T
+}
+
+// NewHashTable creates and returns a new hash map
+func NewHashTable[T comparable]() *HashTable[T] {
+	return &HashTable[T]{
+		Table: make(map[string]T),
+	}
+}
