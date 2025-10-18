446d92bb3b022982559211b25a6f96268fe0fbb4
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-18 18:05:31 -0300

    FEAT: adds hash function and a method that calls it

--- dictionary/hashTable.go
+++ dictionary/hashTable.go
@@ -11,3 +11,17 @@ func NewHashTable[T comparable]() *HashTable[T] {
 		Table: make(map[string]T),
 	}
 }
+
+// loseloseHashCode is a simple hash function
+func loseloseHashCode(key string) int {
+	hash := 0
+	for i := 0; i < len(key); i++ {
+		hash += int(key[i])
+	}
+	return hash % 37
+}
+
+// HashCode returns the hash code for a given key
+func (h *HashTable[T]) HashCode(key string) int {
+	return loseloseHashCode(key)
+}
