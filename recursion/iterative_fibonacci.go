71d5e26b9a0e71e35fbae72a53a4927e47f71b7c
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-11 14:26:45 -0300

    feat: adds iterative Fibonacci function

--- recursion/iterative_fibonacci.go
+++ recursion/iterative_fibonacci.go
@@ -1,1 +1,23 @@
 package recursion
+
+func IterativeFibonacci(n int) int {
+	if n < 1 {
+		return 0
+	}
+
+	if n <= 2 {
+		return 1
+	}
+
+	fibNMinus2 := 0
+	fibNMinus1 := 1
+	fibN := n
+
+	for i := 2; i <= n; i++ {
+		fibN = fibNMinus1 + fibNMinus2
+		fibNMinus2 = fibNMinus1
+		fibNMinus1 = fibN
+	}
+
+	return fibN
+}
